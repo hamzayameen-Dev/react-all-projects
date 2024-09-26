63d8dabec86c2dfbebc598c050916b93791de8aa
hamza-yameen <hamzayameen3000@gmail.com>
2024-09-27 02:52:01 +0500

    add unsplash project

--- src/Data.js
+++ src/Data.js
@@ -1,30 +1,40 @@
-import accordionProject from './assets/projects/accordion.png'
-import comingSoon from './assets/projects/coming-soon-img.png'
-import menuProject from "./assets/projects/menu.png"
+import accordionProject from "./assets/projects/accordion.png";
+import comingSoon from "./assets/projects/coming-soon-img.png";
+import menuProject from "./assets/projects/menu.png";
+import unsplashImagesProject from "./assets/projects/unsplash-images.png";
 
 const projectsList = [
-  {
-    id: 1,
-    title: 'Accordion',
-    bgImage: accordionProject,
-    website_link: 'https://accordion-react-js-project.vercel.app/',
-    github_link:
-      'https://github.com/hamza-yameen/ReactJs-projects/tree/main/01-accordion',
-  },
-  {
-    id: 2,
-    title: 'Menu',
-    bgImage: menuProject,
-    website_link: 'https://menu-react-js-project.vercel.app/',
-    github_link: 'https://github.com/hamza-yameen/ReactJs-projects/tree/main/02-menu',
-  },
-  {
-    id: 3,
-    title: 'Unsplash Images',
-    bgImage: comingSoon,
-    website_link: '',
-    github_link: '',
-  },
-]
+	{
+		id: 1,
+		title: "Accordion",
+		bgImage: accordionProject,
+		website_link: "https://accordion-react-js-project.vercel.app/",
+		github_link:
+			"https://github.com/hamza-yameen/ReactJs-projects/tree/main/01-accordion",
+	},
+	{
+		id: 2,
+		title: "Menu",
+		bgImage: menuProject,
+		website_link: "https://menu-react-js-project.vercel.app/",
+		github_link:
+			"https://github.com/hamza-yameen/ReactJs-projects/tree/main/02-menu",
+	},
+	{
+		id: 3,
+		title: "Unsplash Images",
+		bgImage: unsplashImagesProject,
+		website_link: null,
+		github_link:
+			"https://github.com/hamza-yameen/ReactJs-projects/tree/main/03-unsplash-images",
+	},
+	{
+		id: 4,
+		title: "Food Cart",
+		bgImage: comingSoon,
+		website_link: "dummy",
+		github_link: "dummy",
+	},
+];
 
-export default projectsList
+export default projectsList;
